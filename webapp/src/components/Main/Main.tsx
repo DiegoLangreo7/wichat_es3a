91dad9d6d37199fa07d9263ebee8b0f4969bd5c9
holmess - Sara Fernández González - UO288195 <sarafergonza@gmail.com>
2025-02-28 17:36:23 +0100

    Eliminar warnings

--- webapp/src/components/Main/Main.tsx
+++ webapp/src/components/Main/Main.tsx
@@ -1,23 +1,7 @@
-import React, { useState } from 'react';
-import axios, {AxiosError} from 'axios';
-import {Container, Box, Button} from '@mui/material';
-import {ErrorResponse} from '../ErrorInterface';
-import { useNavigate } from 'react-router-dom';
+import {Box, Button} from '@mui/material';
 import NavBar from "./items/NavBar";
 
-const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
-
 const Main = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [openSnackbar, setOpenSnackbar] = useState(false);
-
-    const navigate = useNavigate();
-
-    const handleCloseSnackbar = () => {
-        setOpenSnackbar(false);
-    };
 
     return (
         <Box component="main" sx={{
