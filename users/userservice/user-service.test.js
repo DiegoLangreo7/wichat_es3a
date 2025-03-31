2371db94543ebc6d0a0870d20b0ef5ef1a02c82d
DanielAB95 <danielalvarez.dab@gmail.com>
2025-03-31 12:57:22 +0200

    Test user-service (Prueba 1)

--- users/userservice/user-service.test.js
+++ users/userservice/user-service.test.js
@@ -9,10 +9,10 @@ let mongoServer;
 let app;
 
 beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create();
-  const mongoUri = mongoServer.getUri();
-  process.env.MONGODB_URI = mongoUri;
-  app = require('./user-service'); 
+    mongoServer = await MongoMemoryServer.create();
+    const mongoUri = mongoServer.getUri();
+    process.env.MONGODB_URI = mongoUri;
+    app = require('./user-service');
 });
 
 afterAll(async () => {
@@ -21,25 +21,25 @@ afterAll(async () => {
 });
 
 describe('User Service', () => {
-  it('should add a new user on POST /adduser', async () => {
-    const newUser = {
-      username: 'testuser',
-      password: 'testpassword',
-    };
+    it('should add a new user on POST /adduser', async () => {
+        const newUser = {
+            username: 'testuser',
+            password: '123456q@',
+        };
 
-    const response = await request(app).post('/adduser').send(newUser);
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('username', 'testuser');
+        const response = await request(app).post('/adduser').send(newUser);
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('username', 'testuser');
 
-    // Check if the user is inserted into the database
-    const userInDb = await User.findOne({ username: 'testuser' });
+        // Check if the user is inserted into the database
+        const userInDb = await User.findOne({ username: 'testuser' });
 
-    // Assert that the user exists in the database
-    expect(userInDb).not.toBeNull();
-    expect(userInDb.username).toBe('testuser');
+        // Assert that the user exists in the database
+        expect(userInDb).not.toBeNull();
+        expect(userInDb.username).toBe('testuser');
 
-    // Assert that the password is encrypted
-    const isPasswordValid = await bcrypt.compare('testpassword', userInDb.password);
-    expect(isPasswordValid).toBe(true);
-  });
+        // Assert that the password is encrypted
+        const isPasswordValid = await bcrypt.compare('testpassword', userInDb.password);
+        expect(isPasswordValid).toBe(true);
+    });
 });
