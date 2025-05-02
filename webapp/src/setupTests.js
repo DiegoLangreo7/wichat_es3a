e70172309a722a5f4a7798eaab7b5eaea9a19dcb
diego <uo294255@uniovi.es>
2025-05-02 22:44:43 +0200

    Apaño :C

--- webapp/src/setupTests.js
+++ webapp/src/setupTests.js
@@ -1,21 +1,23 @@
-// setupTests.js
 import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
-// Mock global para window.matchMedia
+// Mock global para matchMedia (RetroRain)
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
   value: jest.fn().mockImplementation(query => ({
     matches: false,
     media: query,
     onchange: null,
-    addListener: jest.fn(),
-    removeListener: jest.fn(),
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
     addEventListener: jest.fn(),
     removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
+    dispatchEvent: jest.fn()
+  }))
 });
+
+// Opcional: evitar errores por scrollIntoView en tests con scroll
+Element.prototype.scrollIntoView = jest.fn();
