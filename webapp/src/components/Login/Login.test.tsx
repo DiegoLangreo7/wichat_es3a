16a689b5fb798dc9938c697e36f80367cb3f30e6
diego <uo294255@uniovi.es>
2025-05-02 22:16:57 +0200

    Intento de colar la animacion a los test

--- webapp/src/components/Login/Login.test.tsx
+++ webapp/src/components/Login/Login.test.tsx
@@ -13,6 +13,24 @@ jest.mock('react-router', () => ({
     useNavigate: () => mockNavigate,
 }));
 
+// Mock de window.matchMedia para Jest
+beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+        writable: true,
+        value: jest.fn().mockImplementation(query => ({
+            matches: false,
+            media: query,
+            onchange: null,
+            addListener: jest.fn(),
+            removeListener: jest.fn(),
+            addEventListener: jest.fn(),
+            removeEventListener: jest.fn(),
+            dispatchEvent: jest.fn(),
+        })),
+    });
+});
+
+
 describe('Login component', () => {
     beforeEach(() => {
         mockAxios.reset();
